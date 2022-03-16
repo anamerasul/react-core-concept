12619fb35341791a2037f8456bc135bcb305c1f8
anamerasul <anamerasul@gmail.com>
2022-03-16 18:54:27 +0600

    JSX, Dynamic content, Dynamic Style in React

--- react-core-concept-yarn/src/App.js
+++ react-core-concept-yarn/src/App.js
@@ -1,12 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+const num=555;
+
+const singer ={
+  name:'Dr mahafux',job:'singer'
+}
+const singer2 ={
+  name:'Dr mahafux 2',job:'singer 2'
+}
+
+
+const singerStyle={
+
+  color:'purple',
+  backgroundColor:'gray'
+
+} 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>JSX</h1>
+        <div className="container">
+          <h3>Hello dude</h3>
+          </div>
+
+          <div className="music" id="music">
+           <p>Name:{222+num}</p>
+           <p style={singerStyle}>{singer.name}</p>
+           <p>{singer.job}</p>
+           <p>{singer2.name}</p>
+           <p style={{color:'blue',backgroundColor:'yellow'}}>{singer2.job}</p>
+            </div>
+        <p></p>
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>JSX</h1>
         <p>
+
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
