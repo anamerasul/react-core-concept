1dd4310b407deb1b6a4d291f2879206cc87a9143
anamerasul <anamerasul@gmail.com>
2022-03-17 18:03:30 +0600

    Concept Recap, JSX, components, props, display array of objects

---
+++ react-js-core-concept2/src/App.js
@@ -0,0 +1,40 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+
+  const products=[
+    {name:'laptop',price:167000},
+    {name:'phone',price:30000},
+    {name:'watch',price:7000},
+    {name:'sunglass',price:1000}
+  ]
+  return (
+    <div className="App">
+       {/* <header className="App-header">
+
+      </header> */}
+      {/* <Product name="laptop" price="32000"></Product>
+      <Product name="phone"></Product>
+      <Product name="watch" price='250'></Product> */}
+
+    {/* <Product ></Product> */}
+
+    {
+      products.map(product=><Product name={product.name} price={product.price}></Product>)
+    }
+    </div>
+  );
+}
+
+
+function Product(props){
+  return(
+    <div className='product'>
+      <h3>Name:{props.name}</h3>
+      <p>price:{props.price}</p>
+    </div>
+  )
+}
+export default App;
